d882eeae1b7cc626f2c57e47b60a87dc9f102351
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-09-24 13:59:53 -0400

    feat: Navbar state added
    Changes the color of the selection on the navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,16 +1,35 @@
 import React from 'react'
 
 export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedCategory: 'Top'
+        }
+
+        this.updateCategory = this.updateCategory.bind(this)
+    }
+
+    updateCategory(selectedCategory) {
+        this.setState({
+            selectedCategory
+        })
+    }
+
     render() {
 
-        const selections = ['Top', 'New']
+        const categories = ['Top', 'New']
 
         return (
             <ul className='flex-center'>
-                {selections.map((selection) => (
-                    <li key={selection}>
-                        <button className='btn-clear nav-link'>
-                            {selection}
+                {categories.map((category) => (
+                    <li key={category}>
+                        <button
+                            className='btn-clear nav-link'
+                            style={category === this.state.selectedCategory ? {color: 'rgb(187, 46, 31)'} : null}
+                            onClick={() => this.updateCategory(category)}>
+                            {category}
                         </button>
                     </li>
                 ))}
